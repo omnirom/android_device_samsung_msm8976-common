a97da9c6e846527aa3ab1c487687500649529a60
Menno van Grinsven <scanno71@gmail.com>
2017-03-04 21:56:53 +0100

    Add charger resources
    Change-Id: Iae96ca5e0eb6e8a8f2b7e1d7ce4c72fbcc2169ad

--- msm8976.mk
+++ msm8976.mk
@@ -59,6 +59,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
